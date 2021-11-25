9dcbcc3c1682ba7a84a73792a10494c4d73f4226
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2021-11-24 22:01:05 -0300

    Adição de itens

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+export const items = [
+      { date: new Date(2021,11, 4), category: 'food', title: 'McDonalds', value: 32.12},
+      { date: new Date(2021,11, 4), category: 'food', title: 'Burguer King', value: 28},
+      { date: new Date(2021,11, 6), category: 'rent', title: 'Aluguel apt', value: 2600},
+      { date: new Date(2021,12, 18), category: 'salary', title: 'Salário', value: 12000},
+];
